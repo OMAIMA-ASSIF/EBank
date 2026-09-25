3fa6fedd345fe2232fa5f00ceb666f2c7c3d7ff0
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-09-26 00:12:34 +0100

    web layer

---
+++ bank-account-service/src/main/java/net/omaima/bankaccountservice/web/AccountRestController.java
@@ -0,0 +1,41 @@
+package net.omaima.bankaccountservice.web;
+
+import net.omaima.bankaccountservice.entities.BankAccount;
+import net.omaima.bankaccountservice.repositories.BankAccountRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+public class AccountRestController {
+    private BankAccountRepository bankAccountRepository;
+
+    public AccountRestController(BankAccountRepository bankAccountRepository){
+        this.bankAccountRepository = bankAccountRepository;
+    }
+
+    @GetMapping("/bankAccounts/{id}")
+    public BankAccount bankAccount(@PathVariable String id){
+        return bankAccountRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException(String.format("Account %s not found ", id)));
+    }
+
+    @PostMapping("/bankAccounts")
+    public BankAccount save(@RequestBody BankAccount bankAccount){
+        //normalement had l condition khsha tkoun f l business layer
+        if(bankAccount.getId()==null) bankAccount.setId(UUID.randomUUID().toString());
+        return bankAccountRepository.save(bankAccount);
+    }
+
+    @PutMapping("/bankAccounts/{id}")
+    public BankAccount update(@PathVariable String id, @RequestBody BankAccount bankAccount){
+        BankAccount account = bankAccountRepository.findById(id).orElseThrow();
+        if(bankAccount.getBalance()!=null) account.setBalance(bankAccount.getBalance());
+        if(bankAccount.getCreateAt()!=null) account.setCreateAt(bankAccount.getCreateAt());
+        if(bankAccount.getType()!=null) account.setType(bankAccount.getType());
+        if(bankAccount.getCurrency()!=null) account.setCurrency(bankAccount.getCurrency());
+
+        return bankAccountRepository.save(account);
+    }
+
+
+}
